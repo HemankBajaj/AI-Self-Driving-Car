1fd7b2adeada225c4bc19ec9a51d8ee7b9d7b68f
HemankBajaj <hemankbajaj05@gmail.com>
2022-11-13 19:03:16 +0530

    planner added

--- intelligentDriver.py
+++ intelligentDriver.py
@@ -13,6 +13,8 @@ from engine.model.car.car import Car
 from engine.model.layout import Layout
 from engine.model.car.junior import Junior
 from configparser import InterpolationMissingOptionError
+import math 
+import random
 
 # Class: Graph
 # -------------
@@ -22,6 +24,13 @@ class Graph(object):
         self.nodes = nodes
         self.edges = edges
 
+    def getNbrs(self, node):
+        nbrs = []
+        for edge in self.edges:
+            if edge[0][0] == node[0] and edge[0][1] == node[1]:
+                if edge[1] not in nbrs:
+                    nbrs.append(edge[1])
+        return nbrs
 # Class: IntelligentDriver
 # ---------------------
 # An intelligent driver that avoids collisions while visiting the given goal locations (or checkpoints) sequentially. 
@@ -35,7 +44,7 @@ class IntelligentDriver(Junior):
         self.worldGraph = self.createWorldGraph()
         self.checkPoints = self.layout.getCheckPoints() # a list of single tile locations corresponding to each checkpoint
         self.transProb = util.loadTransProb()
-        
+
     # ONE POSSIBLE WAY OF REPRESENTING THE GRID WORLD. FEEL FREE TO CREATE YOUR OWN REPRESENTATION.
     # Function: Create World Graph
     # ---------------------
@@ -117,7 +126,140 @@ class IntelligentDriver(Junior):
 
         currPos = self.getPos() # the current 2D location of the AutoCar (refer util.py to convert it to tile (or grid cell) coordinate)
         # BEGIN_YOUR_CODE 
+        # print("currpos", currPos)
+        # nextGoal = self.checkPoints[chkPtsSoFar]
+        def euclideanDistance(x1, y1, x2, y2):
+            return math.sqrt((x1-x2)**2 + (y1-y2)**2)
+
+        def manhattanDistance(x1, y1, x2, y2):
+            return abs(x1-x2) + abs(y1-y2)
+        numRows, numCols = self.layout.getBeliefRows(), self.layout.getBeliefCols()
+
+        print("RC->", self.layout.getBeliefRows(), self.layout.getBeliefCols())
+        dxList = [-1, 0, +1]
+        dyList = [-1, 0, +1]
+
+        def isBlockInVicinity(xPos, yPos): # True if block nearby
+            for dx in dxList:
+                for dy in dyList:
+                    x = xPos + 1.3*dx*Car.LENGTH 
+                    y = yPos + 1.3*dy*Car.LENGTH 
+                    row = util.yToRow(y); col = util.xToCol(x)
+                    if (row, col) not in self.worldGraph.nodes:
+                        return True 
+            return False 
+
+        ## Check if there is 5% chance 
+        def isCarInVicinity(row, col): # True if there is a car nearby
+            for dx in dxList:
+                for dy in dyList:
+                    r = row + dx 
+                    c = col + dy
+                    p = 0
+                    for belief in beliefOfOtherCars:
+                        p += belief.getProb(r, c)
+                    if p > 0.02 * len(beliefOfOtherCars):
+                        return True
+            return False 
+    
+        def isCar(x, y):
+            row = util.yToRow(y); col = util.xToCol(x)
+            p = 0
+            for belief in beliefOfOtherCars:
+                p += belief.getProb(row, col)
+            if p > 0.02 * len(beliefOfOtherCars):
+                return True
+            return False
+
+        def isBlock(x, y):
+            row = util.yToRow(y); col = util.xToCol(x)
+            if (row, col) not in self.worldGraph.nodes:
+                return True
+            return False
+        def isBlockCell(row, col):
+            if (row, col) not in self.worldGraph.nodes:
+                return True
+            return False
+        def getProbCar(row, col):
+            p = 0
+            for belief in beliefOfOtherCars:
+                p += belief.getProb(row, col)
+            return p
+        def numberOfBlocksInvicinity(row, col):
+            cnt = 0
+            for dx in dxList:
+                for dy in dyList:
+                    x = row + dx 
+                    y = col + dy  
+                    if (row, col) not in self.worldGraph.nodes:
+                        cnt += 1
+            return cnt 
+        nextGoal = self.checkPoints[chkPtsSoFar]
+        x0 = currPos[0]; x1 = currPos[1]
         
+        currNode = (util.yToRow(x1), util.xToCol(x0) )
+
+        minDist = 1e9  # set to inf
+        print("Curr", currNode)
+        goalNode = None 
+        for dx in dxList:
+            for dy in dyList:
+                if dx != 0 or dy != 0:
+                    x = currPos[0] + dx*1.5*Car.LENGTH 
+                    y = currPos[1] + dy*1.5*Car.LENGTH 
+                    row = util.yToRow(y); col = util.xToCol(x)
+                    if (row , col) in self.worldGraph.nodes and row < numRows and col < numCols:
+                        dist =  manhattanDistance(row, col, nextGoal[0], nextGoal[1])
+                        print("ROW->", row, "COL->", col)
+
+                        ## Check this condn
+                        if row < numRows-1 and col < numCols-1 and not isCarInVicinity(row, col) and not isBlockInVicinity(x, y): # less than 5% chance that a car is there
+                            if dist <= minDist:
+                                goalNode = (row, col)
+                                minDist = dist 
+
+        
+        if goalNode == None:
+            print("Goal Node not Decided")
+            cond = currNode[0] > 0 and currNode[1] > 1 and currNode[0] < numRows - 1 and currNode[1] < numCols - 1
+            if cond and isCarInVicinity(currNode[0], currNode[1]):
+                print("Car in Vicinity")
+                minProb = len(beliefOfOtherCars)
+                for dx in dxList:
+                    for dy in dyList:
+                        tempX = currPos[0] + dx*1.5*Car.LENGTH
+                        tempY = currPos[1] + dy*1.5*Car.LENGTH
+                        tempNode = ( util.yToRow(tempY),util.xToCol(tempX) )
+                        condn = tempNode[0] < numRows -1 and tempNode[1] < numCols-1 and tempNode[0] >= 1 and tempNode[1] >=1
+                        if condn and tempNode in self.worldGraph.nodes and  minProb > getProbCar(tempNode[0], tempNode[1]) and not isBlockCell(tempNode[0], tempNode[1]):
+                            minProb = getProbCar(tempNode[0], tempNode[1])
+                            goalPos = (tempX, tempY)
+                            moveForward = True
+            else:
+                print("Block in vicinity not Car")
+                for dx in dxList:
+                    for dy in dyList:
+                        # print("Error aane wala hai ?")
+                        row = currNode[0]+dx
+                        col = currNode[1]+dy  
+                        condn = row >= 0 and col >= 0  and row < numCols-1 and col  < numCols-1                   
+                        minDist = 1e9
+                        cntBlk = 10 
+                        if condn  and (dx != 0 or dy != 0) and (row, col) in self.worldGraph.nodes and not isBlockCell(row, col) and not isCarInVicinity(row, col):
+                                dist = manhattanDistance(row, col, nextGoal[0], nextGoal[1])
+                                cnt = numberOfBlocksInvicinity(row, col)
+                                print(cnt)
+                                if cnt < cntBlk or (cnt == cntBlk and minDist > dist):
+                                    cntBlk = cnt
+                                    minDist = dist
+                                    goalPos = (util.colToX(col), util.rowToY(row))
+                                    moveForward = True
+                if goalNode == None:
+                    moveForward = False 
+        else:
+            goalPos = (util.colToX(goalNode[1]), util.rowToY(goalNode[0]))
+
+
         # END_YOUR_CODE
         return goalPos, moveForward
 
@@ -138,7 +280,8 @@ class IntelligentDriver(Junior):
             Car.TURN_WHEEL: wheelAngle
         }
         if driveForward:
-            actions[Car.DRIVE_FORWARD] = 1.0
+            actions[Car.DRIVE_FORWARD] = 1
         return actions
     
     
+
